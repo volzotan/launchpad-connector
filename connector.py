35a4ebc0b78c66f0534787c595ba052480ba3b99
Christopher Getschmann <christopher@volzo.de>
2015-01-27 00:13:03 +0100

    init animation

--- connector.py
+++ connector.py
@@ -24,6 +24,8 @@ class Connector(object):
         self.lp = Launchpad()
         self.lp.reset()
 
+        self.init_animation()
+
 
     def load_all_modules_from_dir(self, dirname):
         for importer, package_name, _ in pkgutil.iter_modules([dirname]):
@@ -41,6 +43,45 @@ class Connector(object):
         for elem in self.sub:
             self.handlers.append(elem.consume)
 
+
+    def init_animation(self):
+        import random
+        from random import randint
+
+        PAUSE_DURATION = 0.2
+
+        # calculate random order 
+        btn_list = []
+        for x in range (0,9):
+            for y in range(0,9):
+                btn_list.append((x,y))
+        random.shuffle(btn_list)
+
+        for color in range(1,4):
+            for elem in btn_list:
+                green = (color & 2) >> 1
+                red = color & 1
+                # first digit and second digit [0|1, 1|0, 1|1, (0|0)]
+                self.lp.lightButton(elem, green, red)
+                time.sleep(0.003)
+
+            # for brightness in range(1,4):
+            #     for x in range(0,8):
+            #         for y in range(0,8):
+            #             self.lp.lightButton((x,y), green * brightness, red * brightness)
+            #     time.sleep(0.05)
+
+            time.sleep(PAUSE_DURATION)
+
+        for i in range(0,400):
+            self.lp.lightButton((randint(0,7), randint(0,7)), randint(0,3), randint(0,3))
+            time.sleep(0.003)
+
+        for elem in btn_list:
+            self.lp.lightButton(elem, 0, 0)
+            time.sleep(0.003)
+
+        time.sleep(1)
 
     def loop(self):
         logger.info("event loop running")
